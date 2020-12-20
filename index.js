404263b2d177e22c1b8e457c2a1c2615f049fa63
Hyevvy <72402747+Hyevvy@users.noreply.github.com>
2020-12-21 03:03:04 +0900

    Add files via upload
    ⭐️마우스 커서가 index.html 의 profileSnsIcons에 올라갔을 때 크기가 바뀌는 효과를 index.js를 통해 구현함.

--- index.js
+++ index.js
@@ -11,3 +11,19 @@ $(document).ready(function(){
   
    
  }); 
+
+
+ 
+function zoomIn(event) {
+  //sns아이콘에 마우스 커서가 올라왔을때
+  event.target.style.width = "70px";
+  event.target.style.height = "70px";
+  event.target.style.transition = "all 0.5s";
+}
+
+function zoomOut(event) {
+  //sns아이콘에 올라갔던 마우스 커서가 다시 나올 때 
+  event.target.style.width = "50px";
+  event.target.style.height = "50px";
+  event.target.style.transition = "all 0.5s";
+}
