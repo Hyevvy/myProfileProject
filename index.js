d83fb4c8fb9a32e8cd7ffeec1f83ce42c6e06405
Hyevvy <72402747+Hyevvy@users.noreply.github.com>
2021-01-05 23:36:15 +0900

    [fix] 20210105
    onload되자마자 2초뒤에 보라색으로 바뀌었다 4초 뒤 다시 검정색으로 바뀜

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 $(document).ready(function(){ 
     var changedTextcolor = document.querySelectorAll('.profileChangedName');
     setTimeout(function() {
-        console.log('Works!');
-      }, 10000);
-
-    for (var i=0; i<changedTextcolor.length; i++ ){
+      for (var i=0; i<changedTextcolor.length; i++ ){
         var text = changedTextcolor.item(i);
         text.style.color="violet";
+        setTimeout(function() {
+          for (var i=0; i<changedTextcolor.length; i++ ){
+            var text = changedTextcolor.item(i);
+            text.style.color="black";
+        }
+          }, 4000);
+    
     }
+      }, 1000);
+
+    
   
    
  }); 
